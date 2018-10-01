eaf64a6760fa7f9065890ee98f407badc4edd161
Call-Jacob <dehholio@gmail.com>
2018-10-01 16:38:30 -0600

    Group work

---
+++ web/results.php
@@ -0,0 +1,25 @@
+<?php
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$major = htmlspecialchars($_POST["major"]);
+$comments = htmlspecialchars($_POST["comments"]);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Submission Results</title>
+    </head>
+    
+    <body>
+        <h1>Your Results:</h1>
+        
+        <p>Name: <?=$name ?></p>
+        <p>Email: <?=$email ?></p>
+        <p>Major: <?=$major ?></p>
+        <p>Comments: <?=$comments ?></p>
+    </body>
+    
+    
+</html>
