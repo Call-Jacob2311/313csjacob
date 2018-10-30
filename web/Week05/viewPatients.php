a83bed9fe3f2c701ca310ba5aae5b1bdf793f585
Call-Jacob <dehholio@gmail.com>
2018-10-29 22:06:25 -0600

    Fixed Week05 confirmation and Index

--- web/Week05/viewPatients.php
+++ web/Week05/viewPatients.php
@@ -1,9 +1,13 @@
 <?php
 require_once 'library/connection.php';
 
-foreach ($db->query('SELECT lastname, firstname FROM patients') as $row)
+foreach ($db->query('SELECT lastname, firstname, ID FROM patients') as $row)
 {
-  echo 'first name: ' . $row['firstname'] . ' ' . $row['lastname'];
+  echo 'Name: ' . $row['firstname'] . ' ' . $row['lastname'] . '        <a href="patient.php" value="' . $row[ID] . '">Click me!</a>';
   echo '<br/><br/>';
 }
 ?>
+
+<button type="button" value="submit" onclick="window.location.href='addPatient.php'">Add new patient</button>
+</br>
+<button type="button" value="Click on Me!!" onclick="window.location.href='index.php'">Return home</button>
