01e4346d4bee07fba59afa98bea8b4f9a4ef5c34
Call-Jacob <dehholio@gmail.com>
2018-10-29 20:08:36 -0600

    Fixed Week05 index error and added AddUser

--- web/Week05/addUser.php
+++ web/Week05/addUser.php
@@ -6,4 +6,9 @@ $firstName = filter_input(INPUT_POST, 'firstname');
 $lastName = filter_input(INPUT_POST, 'lastname');
 $email = filter_input(INPUT_POST, 'email');
 
-echo $firstName;
+$stmt = $db->prepare('INSERT INTO users (lastname, firstname, email) VALUES (:lastName, :firstName, :email)');
+$stmt->bindValue(':lastName', $lastName, PDO::PARAM_STR);
+$stmt->bindValue(':firstName', $firstName, PDO::PARAM_STR);
+$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
