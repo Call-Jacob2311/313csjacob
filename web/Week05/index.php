401ea1d790dd24a44d1fb2eabe5079d37af7b604
Call-Jacob <dehholio@gmail.com>
2018-10-29 19:20:50 -0600

    Fixed Week05 index error

--- web/Week05/index.php
+++ web/Week05/index.php
@@ -9,7 +9,7 @@ require_once 'library/connection.php';
 
 foreach ($db->query('SELECT lastname, firstname, email FROM users') as $row)
 {
-  echo 'first name: ' . $row['firstname'];
+  echo 'first name: ' . $row['firstname'] . '</br>';
   echo 'last name: ' . $row['lastname'];
   echo 'email: ' . $row['email'];
   echo '<br/>';
