91c5e8c241969a3a04a2e4255ed61df1294873d3
Call-Jacob <dehholio@gmail.com>
2018-11-04 22:26:09 -0700

    Added changes to the Week 07 project

---
+++ web/Week07/addGame-confirmation.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'library/connection.php';
+
+$title = filter_input(INPUT_POST, 'title');
+$year = filter_input(INPUT_POST, 'year');
+$status = filter_input(INPUT_POST, 'status');
+$rating = filter_input(INPUT_POST, 'rating');
+
+$stmt = $db->prepare('INSERT INTO game (title, year, status, rating) VALUES (:title, :year, :status, :rating)');
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':year', $year, PDO::PARAM_STR);
+$stmt->bindValue(':status', $status, PDO::PARAM_STR);
+$stmt->bindValue(':rating', $rating, PDO::PARAM_INT);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>GamerBase Version - One</title>
+	<link rel="stylesheet" type="text/css" href="mainCSS.css">
+    </head>
+    
+    <body>
+        <div class="container">
+            <header>
+                <h1>GamerBase</h1>
+            </header>
+
+            <div id="home">
+                <h2>Thank you for adding a game!</h2>
+                <div class="btn-container">
+                    <?php
+                        echo '<span>You have added: ' . $title . ' to the database! </span><br>'
+                    ?>
+                </div>
+            </div>
+
+            <footer>
+                <span>Property of Jacob Call. All code and resources strictly used for school. Work in the making.</span>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ web/Week07/addGame.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>GamerBase Version - One</title>
+	<link rel="stylesheet" type="text/css" href="mainCSS.css">
+    </head>
+    
+    <body>
+        <div class="container">
+            <header>
+                <h1>GamerBase</h1>
+            </header>
+
+            <div id="home">
+                <h2>Add A Game</h2>
+                <div class="btn-container">
+                    <form action="addGame-confirmation.php" method="post">
+                        Title Name: <input type="text" name="title"><br>
+                        Year: <input type="number" name="year" min="1970" max="2019"><br>
+                        Status:<br><input type="radio" name="status" value="complete"> Complete<br>
+                        <input type="radio" name="status" value="playing"> Playing<br>
+                        <input type="radio" name="status" value="to do"> To Do<br> 
+                        Please choose a rating (1-10): <input type="number" name="rating" min="1" max="10"><br>
+                        <input type="submit">
+                    </form>
+                </div>
+            </div>
+
+            <footer>
+                <span>Property of Jacob Call. All code and resources strictly used for school. Work in the making.</span>
+            </footer>
+        </div>
+    </body>
+</html>
