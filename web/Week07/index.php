21129f626966794295a429bd399861447e2a433b
Call-Jacob <dehholio@gmail.com>
2018-11-04 21:09:43 -0700

    Added Week 07 project

---
+++ web/Week07/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	<title>GamerBase Version - One</title>
+	<link rel="stylesheet" type="text/css" href="mainCSS.css">
+    </head>
+    
+    <body>
+        <div class="container">
+            <header>
+                <h1>GamerBase</h1>
+            </header>
+
+            <div id="home">
+                <h2>Welcome to GamerBase</h2>
+                <div class="btn-container">
+                    <button type="button" class="button" value="submit" onclick="window.location.href='addGame.php'">Add A Game</button>
+                    <br>
+                    <br>
+                    <button type="button" class="button" value="submit" onclick="window.location.href='viewGame.php'">View All Game</button>
+                    <br>
+                    <br>
+                    <button type="button" class="button" value="submit" onclick="window.location.href='editGame.php'">Edit / Delete Game</button>
+                    <br>
+                    <br>
+                    <button type="button" class="button" value="submit" onclick="window.location.href='about.php'">About</button>
+                    <br>
+                </div>
+            </div>
+
+            <footer>
+                <span>Property of Jacob Call. All code and resources strictly used for school. Work in the making.</span>
+            </footer>
+        </div>
+    </body>
+</html>
